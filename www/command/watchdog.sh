e24c0ab50a15a026c8b59a4855d98010517e590f
Tim Curtis <tim@moodeaudio.org>
2019-07-07 12:35:20 -0400

    Add patch number to release in comment header

--- www/command/watchdog.sh
+++ www/command/watchdog.sh
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-# 2019-MM-DD TC moOde 5.4
+# 2019-MM-DD TC moOde 5.4.0
 #
 
 FPMLIMIT=40
