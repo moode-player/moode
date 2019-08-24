7a769f1a2750d609c2c73ab7206f2df3be03d3ce
Tim Curtis <tim@moodeaudio.org>
2019-08-24 08:32:13 -0400

    Add "Recently Added" setting

--- www/lop-config.php
+++ www/lop-config.php
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * 2019-08-18 TC moOde 6.1.0
+ * 2019-MM-DD TC moOde 6.2.0
  *
  */
 
@@ -30,6 +30,8 @@ if (isset($_POST['save']) && $_POST['save'] == '1') {
 	// Detect changes requiring cache delete
 	if ($_POST['config']['ignore_articles'] != $_SESSION['ignore_articles'] ||
 		$_POST['config']['library_utf8rep'] != $_SESSION['library_utf8rep'] ||
+
+		// NOTE: Artist and Album sort tags are for future use
 		$_POST['config']['library_artist_sort'] != $_SESSION['library_artist_sort'] ||
 		$_POST['config']['library_album_sort'] != $_SESSION['library_album_sort']) {
 
@@ -53,6 +55,13 @@ $_select['show_genres'] .= "<option value=\"Yes\" " . (($_SESSION['show_genres']
 $_select['show_genres'] .= "<option value=\"No\" " . (($_SESSION['show_genres'] == 'No') ? "selected" : "") . ">No</option>\n";
 // Compilation identifier
 $_select['library_comp_id'] = $_SESSION['library_comp_id'];
+// Recently added
+// NOTE: library_recently_added is in milliseconds, 1 day = 86400000 ms
+$_select['library_recently_added'] .= "<option value=\"604800000\" " . (($_SESSION['library_recently_added'] == '604800000') ? "selected" : "") . ">1 Week</option>\n";
+$_select['library_recently_added'] .= "<option value=\"2592000000\" " . (($_SESSION['library_recently_added'] == '2592000000') ? "selected" : "") . ">1 Month</option>\n";
+$_select['library_recently_added'] .= "<option value=\"7776000000\" " . (($_SESSION['library_recently_added'] == '7776000000') ? "selected" : "") . ">3 Months</option>\n";
+$_select['library_recently_added'] .= "<option value=\"15552000000\" " . (($_SESSION['library_recently_added'] == '15552000000') ? "selected" : "") . ">6 Months</option>\n";
+$_select['library_recently_added'] .= "<option value=\"31536000000\" " . (($_SESSION['library_recently_added'] == '31536000000') ? "selected" : "") . ">1 year</option>\n";
 // Ignore articles
 $_select['ignore_articles'] = empty($_SESSION['ignore_articles']) ? 'None' : $_SESSION['ignore_articles'];
 // Library utf8 replace
@@ -67,6 +76,8 @@ $_select['library_hiresthm'] .= "<option value=\"400px\" " . (($_SESSION['librar
 // Cover search prioroty
 $_select['library_covsearchpri'] .= "<option value=\"Embedded cover\" " . (($_SESSION['library_covsearchpri'] == 'Embedded cover') ? "selected" : "") . ">Embedded cover</option>\n";
 $_select['library_covsearchpri'] .= "<option value=\"Cover image file\" " . (($_SESSION['library_covsearchpri'] == 'Cover image file') ? "selected" : "") . ">Cover image file</option>\n";
+
+// NOTE: Artist and Album sort tags are for future use
 // Artist sort tag
 $_select['library_artist_sort'] .= "<option value=\"Artist\" " . (($_SESSION['library_artist_sort'] == 'Artist') ? "selected" : "") . ">Artist</option>\n";
 $_select['library_artist_sort'] .= "<option value=\"ArtistSort\" " . (($_SESSION['library_artist_sort'] == 'ArtistSort') ? "selected" : "") . ">ArtistSort</option>\n";
