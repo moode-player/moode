d55834a00489cd872cc89ff705278f9d9df3fa70
Tim Curtis <tim@moodeaudio.org>
2020-04-16 15:48:42 -0400

    Scroll radio covers to middle of panel

--- www/js/playerlib.js
+++ www/js/playerlib.js
@@ -1641,7 +1641,7 @@ function customScroll(list, itemNum, speed) {
 	else if (list == 'radiocovers') {
 		listSelector = '#database-radio';
 		scrollSelector = listSelector;
-		chDivisor = 6;
+		chDivisor = 60;
 	}
 
 	// item position
