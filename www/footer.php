530a0f65af223bb9901154aea3629dc6aa434090
Tim Curtis <tim@moodeaudio.org>
2020-07-05 08:13:10 -0400

    Remove spurious tab

--- www/footer.php
+++ www/footer.php
@@ -32,7 +32,7 @@
 			<h4>Release Information</h4>
 			<ul>
 				<li>Release: 6.6.0 2020-MM-DD</li> <!-- NOTE: getMoodeRel() parses this  -->
-				<li>Coding:	Tim Curtis &copy; 2014</li>
+				<li>Coding: Tim Curtis &copy; 2014</li>
 				<li>Documentation: <a class="moode-about-link" href="./relnotes.txt" target="_blank">View release notes,</a>&nbsp<a class="moode-about-link" href="./setup.txt" target="_blank">View setup guide</a></li>
 				<li>Contributors: <a class="moode-about-link" href="./CONTRIBS.html" target="_blank">View contributors</a></li>
 				<li>License: <a class="moode-about-link" href="./COPYING.html" target="_blank">View GPLv3</a></li>
