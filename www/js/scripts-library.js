74591d8a0ba82047c039f2e06ee77041f96b7ad3
scripple <scripple@users.noreply.github.com>
2020-09-25 10:44:19 -0700

    Make clicking an expanded album toggle back to showing just the selected artist.

--- www/js/scripts-library.js
+++ www/js/scripts-library.js
@@ -942,21 +942,32 @@ $('#albumsList').on('click', '.lib-entry', function(e) {
 	UI.libPos[1] = filteredAlbumCovers.map(function(e) {return e.key;}).indexOf(filteredAlbums[pos].key);
 	var albumobj = filteredAlbums[pos];
 	var album = filteredAlbums[pos].album;
-	// Store the active state before it gets set below
+	// Store the active/expanded states before they gets set below
         var alreadyActive = this.className.includes('active')
+        var alreadyExpanded = this.className.includes('expanded')
 	storeLibPos(UI.libPos);
 	$('#albumsList .lib-entry').removeClass('active');
+	$('#albumsList .lib-entry').removeClass('expanded');
 	$('#albumsList .lib-entry').eq(pos).addClass('active');
 	// If a compilation album is already selected but for only a subset
 	// of the artists such that some tracks are not show clicking the
 	// album twice (or once after it is set active) will cause the full
 	// track list for the album to populate the song list
-        if(alreadyActive && LIB.filters.artists.length && !LIB.filters.artists.includes(albumobj.artist)) {
-		LIB.filters.artists.push(albumobj.artist);
-		filterSongs();
-		LIB.filters.artists.pop();
-		renderSongs()
-        } else {
+	// Clicking again will contract back to just the artist
+        if (alreadyActive && LIB.filters.artists.length && !LIB.filters.artists.includes(albumobj.artist)) {
+		if(alreadyExpanded) {
+			filterSongs();
+			renderSongs()
+		} 
+		else {
+			$('#albumsList .lib-entry').eq(pos).addClass('expanded');
+			LIB.filters.artists.push(albumobj.artist);
+			filterSongs();
+			LIB.filters.artists.pop();
+			renderSongs()
+		}
+        }
+        else {
 		clickedLibItem(e, keyAlbum(albumobj), LIB.filters.albums, renderSongs);
 	}
 	$('#bottom-row').css('display', 'flex')
@@ -989,6 +1000,7 @@ $('#random-album').click(function(e) {
 	storeLibPos(UI.libPos);
 
 	$(itemSelector).removeClass('active');
+	$(itemSelector).removeClass('expanded');
 	$(itemSelector).eq(pos).addClass('active');
 	customScroll(scrollSelector, pos, 200);
 
