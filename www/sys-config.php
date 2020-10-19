743e1841619ccfcc9d6c6a04939dfab838bda272
Tim Curtis <tim@moodeaudio.org>
2020-10-19 18:31:25 -0400

    Improve help text for 64-bit kernel option

--- www/sys-config.php
+++ www/sys-config.php
@@ -19,7 +19,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * 2020-07-22 TC moOde 6.7.1
+ * 2020-MM-DD TC moOde 7.0.0
  *
  */
 
@@ -365,7 +365,7 @@ if ($_SESSION['feat_bitmask'] & FEAT_KERNEL) {
 	$name = $_SESSION['hdwrrev'];
 	// Pi-2B rev 1.2, Allo USBridge SIG, Pi-3B/B+/A+, Pi-4B
 	if ($name == 'Pi-2B 1GB v1.2' || $model == '3' || $model == '4' || $name == 'Allo USBridge SIG [CM3+ Lite 1GB v1.0]') {
-		$_select['kernel_architecture'] .= "<option value=\"64-bit\" " . (($_SESSION['kernel_architecture'] == '64-bit') ? "selected" : "") . ">64-bit</option>\n";
+		$_select['kernel_architecture'] .= "<option value=\"64-bit\" " . (($_SESSION['kernel_architecture'] == '64-bit') ? "selected" : "") . ">64-bit (Experimental)</option>\n";
 	}
 }
 else {
