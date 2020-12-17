cb0016d5172c4feb2fb094d1f2d5d8f17c37362f
Tim Curtis <tim@moodeaudio.org>
2020-12-17 09:45:46 -0500

    Update hash table

--- var/local/www/db/moode-sqlite3.db.sql
+++ var/local/www/db/moode-sqlite3.db.sql
@@ -1,5 +1,5 @@
 --
--- File generated with SQLiteStudio v3.1.0 on Tue Dec 15 08:53:30 2020
+-- File generated with SQLiteStudio v3.1.0 on Thu Dec 17 09:34:29 2020
 --
 -- Text encoding used: UTF-8
 --
@@ -638,7 +638,7 @@ INSERT INTO cfg_hash (id, param, value, "action") VALUES (6, '/etc/php/7.3/fpm/p
 INSERT INTO cfg_hash (id, param, value, "action") VALUES (7, '/etc/php/7.3/mods-available/opcache.ini', '561a03862bb902fa39ca0b6358eaf320', 'exit');
 INSERT INTO cfg_hash (id, param, value, "action") VALUES (8, '/tmp/cfg_audiodev.sql', 'af0f359120b6b61a00808f69b62e5af7', 'exit');
 INSERT INTO cfg_hash (id, param, value, "action") VALUES (9, '/var/www/command/worker.php', 'c84e3e21f92d6447d72d68735b235562', 'warning');
-INSERT INTO cfg_hash (id, param, value, "action") VALUES (10, '/var/www/inc/playerlib.php', 'a22ac8cb902ccbbf1d3dd06cd015d74e', 'warning');
+INSERT INTO cfg_hash (id, param, value, "action") VALUES (10, '/var/www/inc/playerlib.php', '1261ec0ca46c5d257aee25dc91b82950', 'warning');
 INSERT INTO cfg_hash (id, param, value, "action") VALUES (11, '/var/www/css/main.min.css', '1f38ec11ff165baa85bbdcd1c456e92c', 'warning');
 INSERT INTO cfg_hash (id, param, value, "action") VALUES (12, '/var/www/js/config.min.js', 'eca6629894aa7a4c6b2eb3e19806db60', 'warning');
 INSERT INTO cfg_hash (id, param, value, "action") VALUES (13, '/var/www/js/lib.min.js', '6646045d71b7f1f103789f71bfe430a5', 'warning');
