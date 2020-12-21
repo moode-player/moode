95dd82e3bb6544cf338f383416145f972e056c7d
Tim Curtis <tim@moodeaudio.org>
2020-12-21 11:42:24 -0500

    Bump the release to 7.0.1

--- www/footer.php
+++ www/footer.php
@@ -19,7 +19,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * 2020-12-15 TC moOde 7.0.0
+ * 2020-MM-DD TC moOde 7.0.1
  *
  */
 -->
@@ -31,7 +31,7 @@
 			<p>Moode Audio Player is a derivative of the wonderful WebUI audio player client for MPD originally designed and coded by Andrea Coiutti and Simone De Gregori, and subsequently enhanced by early efforts from the RaspyFi/Volumio projects.</p>
 			<h4>Release Information</h4>
 			<ul>
-				<li>Release: 7.0.0 2020-12-15</li> <!-- NOTE: getMoodeRel() parses this  -->
+				<li>Release: 7.0.1 2020-MM-DD</li> <!-- NOTE: getMoodeRel() parses this  -->
 				<li>Coding: Tim Curtis &copy; 2014</li>
 				<li>Documentation: <a class="moode-about-link" href="./relnotes.txt" target="_blank">View release notes,</a>&nbsp<a class="moode-about-link" href="./setup.txt" target="_blank">View setup guide</a></li>
 				<li>Contributors: <a class="moode-about-link" href="./CONTRIBS.html" target="_blank">View contributors</a></li>
