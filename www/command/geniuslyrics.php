33c0b344ac23c21dcbea38f2973842fe302fdc98
Stephanowicz <elijah@gmx.de>
2021-01-09 15:12:54 +0100

    Display song-lyrics for current song

--- www/command/geniuslyrics.php
+++ www/command/geniuslyrics.php
@@ -1,5 +1,28 @@
 <?php
-error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
+/*!
+ * moOde audio player (C) 2014 Tim Curtis
+ * http://moodeaudio.org
+ *
+ * (C) 2020 Stephanowicz
+ * https://github.com/Stephanowicz
+ *
+ * This Program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This Program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * 2020-12-15 TC moOde 7.0.0
+ *
+ */
+ error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 $ClientAccessToken = '<<<GENIUS.COM --- CLIENT ACCESS TOKEN>>>';
 
 if(count($argv)>1){
--- www/lyrics.php
+++ www/lyrics.php
@@ -3,6 +3,9 @@
  * moOde audio player (C) 2014 Tim Curtis
  * http://moodeaudio.org
  *
+ * (C) 2020 Stephanowicz
+ * https://github.com/Stephanowicz
+ *
  * This Program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3, or (at your option)
@@ -16,7 +19,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * 2019-11-24 TC moOde 6.4.0
+ * 2020-12-15 TC moOde 7.0.0
  *
  */
 
