2169514067568c5ab3eec10676d5fb07515d42f1
Tim Curtis <tim@moodeaudio.org>
2021-04-08 16:28:29 -0400

    Add a TODO to check if module is in use

--- www/snd-config.php
+++ www/snd-config.php
@@ -75,6 +75,9 @@ if (isset($_POST['update_alsavolume_max'])) {
 // ALSA loopback device
 if (isset($_POST['update_alsa_loopback'])) {
 	if (isset($_POST['alsa_loopback']) && $_POST['alsa_loopback'] != $_SESSION['alsa_loopback']) {
+
+		// TODO: Check to see if module is in use
+
 		submitJob('alsa_loopback', $_POST['alsa_loopback'], 'ALSA loopback ' . $_POST['alsa_loopback'], '');
 		playerSession('write', 'alsa_loopback', $_POST['alsa_loopback']);
 	}
