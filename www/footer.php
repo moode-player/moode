4e4ff3a2fee3e378e1dec38822b7c82c06778c74
Tim Curtis <tim@moodeaudio.org>
2021-08-26 18:30:47 -0400

    Add "Multiroom" to the Configure modal

--- www/footer.php
+++ www/footer.php
@@ -75,6 +75,9 @@
 				<?php if ($_SESSION['feat_bitmask'] & $FEAT_INPSOURCE) { ?>
 					<li><a href="inp-config.php" class="btn btn-small row2-btns">Input source</a></li>
 				<?php } ?>
+				<?php if ($_SESSION['feat_bitmask'] & $FEAT_MULTIROOM) { ?>
+					<li><a href="trx-config.php" class="btn btn-small row2-btns">Multiroom</a></li>
+				<?php } ?>
 			</ul>
 		</div>
 	</div>
