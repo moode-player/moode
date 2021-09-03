75b9b71e42e1136e56d0b463cf62608082e42a21
Tim Curtis <tim@moodeaudio.org>
2021-09-03 10:32:45 -0400

    Add test for DSP's off

--- www/trx-config.php
+++ www/trx-config.php
@@ -144,14 +144,18 @@ foreach ($params as $row) {
     $_cfg_multiroom[$row['param']] = $row['value'];
 }
 
+// Feature and button states
 $_feat_multiroom = $_SESSION['feat_bitmask'] & FEAT_MULTIROOM ? '' : 'hide';
-$_multiroom_tx_disable = $_SESSION['alsa_loopback'] == 'Off' ? 'disabled' : '';
+$_dsp_on = ($_SESSION['crossfeed'] == 'Off' && $_SESSION['eqfa12p'] == 'Off' && $_SESSION['alsaequal'] == 'Off' &&
+	$_SESSION['camilladsp'] == 'off' && $_SESSION['invert_polarity'] == '0') ? false : true;
+$_multiroom_tx_disable = ($_SESSION['alsa_loopback'] == 'Off' || $_dsp_on == true) ? 'disabled' : '';
 $_multiroom_rx_disable = $_SESSION['alsavolume'] == 'none' ? 'disabled' : '';
 $_tx_restart_btn_disable = $_SESSION['multiroom_tx'] == 'Off' ? 'disabled' : '';
 $_rx_restart_btn_disable = $_SESSION['multiroom_rx'] == 'Off' ? 'disabled' : '';
 $_tx_restart_link_disable = $_SESSION['multiroom_tx'] == 'Off' ? 'onclick="return false;"' : '';
 $_rx_restart_link_disable = $_SESSION['multiroom_rx'] == 'Off' ? 'onclick="return false;"' : '';
 $_multiroom_initvol_disable = empty($_SESSION['rx_hostnames']) ? 'disable' : '';
+
 // Sender
 $_select['multiroom_tx1'] .= "<input type=\"radio\" name=\"multiroom_tx\" id=\"toggle_multiroom_tx1\" value=\"On\" " . (($_SESSION['multiroom_tx'] == 'On') ? "checked=\"checked\"" : "") . ">\n";
 $_select['multiroom_tx0'] .= "<input type=\"radio\" name=\"multiroom_tx\" id=\"toggle_multiroom_tx2\" value=\"Off\" " . (($_SESSION['multiroom_tx'] == 'Off') ? "checked=\"checked\"" : "") . ">\n";
