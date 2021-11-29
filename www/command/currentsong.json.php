2c510d50e049fa9f1a9833a9349b4c35913a64f6
Michał Blaumann <michal.blaumann@gmail.com>
2021-11-29 07:59:41 +0100

    currentsong.json command, shorter code

--- www/command/currentsong.json.php
+++ www/command/currentsong.json.php
@@ -3,18 +3,8 @@
 $file = '/var/local/www/currentsong.txt';
 if (file_exists($file)) {
     try {
-        $handle = fopen($file, "r");
-        if ($handle) {
-            $arr = [];
-            while (($line = fgets($handle)) !== false) {
-                $res = preg_match("/^([^=]+)=(.+)/", $line, $matches);
-                if ($res) {
-                    $arr[$matches[1]] = trim($matches[2]);
-                }
-            }
-            fclose($handle);
-            echo json_encode($arr);
-        }
+        preg_match_all('/^(.+?)= ?(.*?)$/m', file_get_contents($file), $matches);
+        print(json_encode(array_combine(array_map('trim', $matches[1]), $matches[2])));
     } catch (\Exception $exception) {
     }
 }
