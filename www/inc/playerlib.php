2f05da3e49db5cc7d476158e975bebd5b1d9f149
Tim Curtis <tim@moodeaudio.org>
2021-11-29 18:23:19 -0500

    Remove dead code

--- www/inc/playerlib.php
+++ www/inc/playerlib.php
@@ -2631,10 +2631,12 @@ function startAirplay() {
 		$logfile = '/dev/null';
 	}
 
+	/* DELETE
 	// Get device num
 	$array = sdbquery('select value from cfg_mpd where param="device"', cfgdb_connect());
 	$device = $array[0]['value'];
-
+	*/
+	
 	if ($_SESSION['audioout'] == 'Bluetooth') {
 		$device = 'btstream';
 	}
