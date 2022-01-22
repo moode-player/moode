cab8a327871282944fd7b7bfa7fa993a7e9d6ee3
Bitlab <bitkeeper@users.noreply.github.com>
2022-01-22 23:53:59 +0100

    Cleanup for bullseye (#480)
    * [other] Thirdparty software is move to the pkbuild repo. Cleanup the thirdparty software in the moode repo
    
    * [etc] Cleanup not needed configurations files
    
    * [etc] dived the config files into 3 groups
    
    * `Added .sed` to files which should be update by using sed.
    * Added `.overwrite` to files of which the owner ship is with moode audio player package.
    
    All other files can be just package and copied .
    
    * Move network files to correct location
    
    * Remove unneed file mpd.socket
    
    * Move boss2 oled display to own package.
    
    * Remove wiringpi
    
    * Remove rotenc based on wiringpi
    
    * Remove MrEngMan install script, replaced by different driver as package
    
    * Remove unneeded journal.conf file
    
    * [fix] Change config.txt and rc.local to overwrite mode (not owned by package)

--- other/camilladsp/gui/readme.txt
+++
@@ -1,2 +0,0 @@
-Running _build.sh in only required when updating the camillagui or related software
-For install just run _install.sh.
--- other/trx/trx-0.6.0/notice.h
+++
@@ -1,7 +0,0 @@
-#ifndef NOTICE_H
-#define NOTICE_H
-
-#define VERSION "trx 0.6.0"
-#define COPYRIGHT "(C) Copyright 2020 Mark Hills <mark@xwax.org>\n" \
-                  "(C) Copyright 2021 Tim Curtis <tim@moodeaudio.org>"
-#endif
