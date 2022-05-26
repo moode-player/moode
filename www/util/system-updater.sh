988f81e8991926db26aabbf60f5f223d855e9f24
Tim Curtis <tim@moodeaudio.org>
2022-05-26 09:23:56 -0400

    Update system-updater.sh

--- www/util/system-updater.sh
+++ www/util/system-updater.sh
@@ -16,37 +16,46 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+# NOTE: Input arg $1 represents the release to be updated and correspondes
+#       to the suffix of the update zip file for example update-r802.zip
+#   EX: /var/www/util/system-updater.sh r802
+#
 
-# $1 = rXY ex: r26
-
-messageLog () {
+message_log () {
 	echo "$1"
 	TIME=$(date +'%Y%m%d %H%M%S')
 	echo "$TIME updater: $1" >> /var/log/moode.log
 }
 
+# Get URL to the update zip
 SQLDB=/var/local/www/db/moode-sqlite3.db
-URL=$(sqlite3 $SQLDB "select value from cfg_system where param='res_software_upd_url'")
+UPD_URL=$(sqlite3 $SQLDB "SELECT value FROM cfg_system WHERE param='res_software_upd_url'")
 
 cd /var/local/www
 
-messageLog "Downloading update package $1"
-wget -q $URL/update-$1.zip -O update-$1.zip
+# Download and unzip
+message_log "Downloading update package $1"
+wget -q $UPD_URL/update-$1.zip -O update-$1.zip
 unzip -q -o update-$1.zip
 rm update-$1.zip
 
+# Install update
 chmod -R 0755 update
 update/install.sh
 
+# Report status
+# NOTE: inplace_upd_applied var is checked and reset in daemon/worker.php
 if [ $? -ne 0 ] ; then
-	$(sqlite3 $SQLDB "update cfg_system set value='0' where param='inplace_upd_applied'")
-	messageLog "Update cancelled"
+	$(sqlite3 $SQLDB "UPDATE cfg_system SET value='0' WHERE param='inplace_upd_applied'")
+	message_log "Update cancelled"
 else
-	wget -q $URL/update-$1.txt -O update-$1.txt
-	$(sqlite3 $SQLDB "update cfg_system set value='1' where param='inplace_upd_applied'")
-	messageLog "Update installed, restart required"
+	# Download update-$1.txt to mark successful update
+	wget -q $UPD_URL/update-$1.txt -O update-$1.txt
+	$(sqlite3 $SQLDB "UPDATE cfg_system SET value='1' WHERE param='inplace_upd_applied'")
+	message_log "Update installed, restart required"
 fi
 
+# Cleanup
 rm -rf update
 
 cd ~/
