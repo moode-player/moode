7492b0384c595d1fb58b11ca83fb27f86ba94d94
Tim Curtis <tim@moodeaudio.org>
2023-01-31 12:33:13 -0500

    Apply CSS to camilla config check message

--- www/snd-config.php
+++ www/snd-config.php
@@ -510,11 +510,11 @@ if ($_SESSION['camilladsp'] != 'off' && $_SESSION['camilladsp'] != 'custom') {
 	$result = $cdsp->checkConfigFile($_SESSION['camilladsp']);
 	$output = implode('<br>', $result['msg']);
 	if ($result['valid'] == CDSP_CHECK_NOTFOUND) {
-		$_camilladsp_config_check = "<span style='color: red'>&#10007;</span> " . $output;
+		$_camilladsp_config_check = '<div class="config-help-static"><span style="color: red">&#10007;</span> ' . $output . '</div>';
 	} else if ($result['valid'] == CDSP_CHECK_VALID) {
-		$_camilladsp_config_check = "<span style='color: green'>&check;</span> " . $output;
+		$_camilladsp_config_check = '<div class="config-help-static"><span style="color: green">&check;</span> ' . $output . '</div>';
 	} else {
-		$_camilladsp_config_check = "<span style='color: red'>&#10007;</span> " . $output;
+		$_camilladsp_config_check = '<div class="config-help-static"><span style="color: red">&#10007;</span> ' . $output . '</div>';
 	}
 }
 // Parametric equalizer
