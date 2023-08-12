d4e7abee1ada4d642bdd7d3d9611416f8ffe1e58
Tim Curtis <tim@moodeaudio.org>
2023-08-12 07:46:43 -0400

    Add "Queue time" to context menus
    Show total play time remaining for tracks in the Queue in a popup notification.

--- www/js/notify.js
+++ www/js/notify.js
@@ -35,6 +35,7 @@ function notify(title, msg, duration = '2_seconds') {
         clear_play_group: 'Playing after Queue cleared',
         queue_item_removed: 'Removed from Queue',
 		queue_item_moved: 'Queue items moved',
+        playqueue_time: 'Play time remaining',
         // Library
         clear_libcache: 'Library cache cleared',
         update_library: 'Updating library...',
