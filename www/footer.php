1db0d87d7321ebfc6205c0c855c819067d9dd2a3
Tim Curtis <tim@moodeaudio.org>
2023-12-01 14:50:53 -0500

    Update release date

--- www/footer.php
+++ www/footer.php
@@ -32,7 +32,7 @@
 		</p>
 		<h5>Release Information</h5>
 		<ul>
-			<li>Release: 8.3.7 2023-11-28</li> <!-- NOTE: getMoodeRel() parses this  -->
+			<li>Release: 8.3.7 2023-12-01</li> <!-- NOTE: getMoodeRel() parses this  -->
 			<li>Maintainer: Tim Curtis &copy; 2014</li>
 			<li>Documentation: <a class="moode-about-link target-blank-link" href="./relnotes.txt" target="_blank">View release notes,</a>&nbsp<a class="moode-about-link target-blank-link" href="./setup.txt" target="_blank">View setup guide</a></li>
 			<li>Contributors:  <a class="moode-about-link target-blank-link" href="./CONTRIBS.html" target="_blank">View contributors</a></li>
