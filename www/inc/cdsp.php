c4147321a90e556eb42e975636386bc5122975da
Tim Curtis <tim@moodeaudio.org>
2024-05-07 10:20:36 -0400

    WIP Use SPDX ID for program headers (/inc)

--- www/inc/cdsp.php
+++ www/inc/cdsp.php
@@ -1,27 +1,13 @@
 <?php
-/**
- * moOde audio player (C) 2014 Tim Curtis
- * http://moodeaudio.org
- *
- * This Program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3, or (at your option)
- * any later version.
- *
- * This Program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
+/*
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * Copyright 2014 The moOde audio player project / Tim Curtis
+ * Copyright 2021 @bitlab (@bitkeeper Git)
+*/
 
-/**
+/*
  * Wrapper for functionality related to the use of CamillaDSP with moOde
- * (C) 2021 @bitlab (@bitkeeper Git)
- */
+*/
 
 require_once __DIR__ . '/common.php';
 
