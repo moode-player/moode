b6cdcac21589f7c47237f7861ce6c959f3ebe993
Tim Curtis <tim@moodeaudio.org>
2024-07-26 08:07:12 -0400

    [analog-clock.js] Add SPDX license header

--- www/js/analog-clock.js
+++ www/js/analog-clock.js
@@ -1,24 +1,16 @@
 "use strict";
 
+/*!
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * Copyright 2022 @Nutul (albertonarduzzi@gmail.com)
+*/
+
+//
 // An Analog clock entirely in JavaScript (no images, just html tags).
 // Can be configured to have the seconds-hand move at 1 second (default)
 // or 1/20th of a second interval, or have the seconds-hand not shown
 // at all.
 //
-// (C) 2022 @Nutul (albertonarduzzi@gmail.com)
-//
-// This Program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 3, or (at your option)
-// any later version.
-//
-// This Program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 const ANALOGCLOCK_REFRESH_INTERVAL_NORMAL = 1000;
 const ANALOGCLOCK_REFRESH_INTERVAL_SMOOTH = 50;
