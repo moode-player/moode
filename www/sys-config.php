76489e81f4189e97acd2e065d89da40d1e12ac62
Tim Curtis <tim@moodeaudio.org>
2024-10-19 11:44:58 -0400

    [sys-config] Add release info to Software Update section

--- www/sys-config.php
+++ www/sys-config.php
@@ -357,6 +357,7 @@ sysCmd('rm /tmp/backup.zip /tmp/moodecfg.ini /tmp/restore.zip /tmp/py.log /tmp/s
 
 // SOFTWARE UPDATE
 
+$_this_mooderel = getMoodeRel('verbose');
 $autoClick = " onchange=\"autoClick('#btn-set-updater-auto-check');\"";
 $_select['updater_auto_check_on']  .= "<input type=\"radio\" name=\"updater_auto_check\" id=\"toggle-updater-auto-check-1\" value=\"On\" " . (($_SESSION['updater_auto_check'] == 'On') ? "checked=\"checked\"" : "") . $autoClick . ">\n";
 $_select['updater_auto_check_off'] .= "<input type=\"radio\" name=\"updater_auto_check\" id=\"toggle-updater-auto-check-2\" value=\"Off\" " . (($_SESSION['updater_auto_check'] == 'Off') ? "checked=\"checked\"" : "") . $autoClick . ">\n";
