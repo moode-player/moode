fa143277798ae52fb02ab2ea7b934ef7224465d3
Tim Curtis <tim@moodeaudio.org>
2024-11-04 10:38:24 -0500

    [multiroom.php] Add missing inc for getAlsaIEC958Device()
    Fixes script abort when function getAlsaIEC958Device() is called.

--- www/inc/multiroom.php
+++ www/inc/multiroom.php
@@ -4,6 +4,7 @@
  * Copyright 2014 The moOde audio player project / Tim Curtis
 */
 
+require_once __DIR__ . '/audio.php';
 require_once __DIR__ . '/common.php';
 require_once __DIR__ . '/session.php';
 require_once __DIR__ . '/sql.php';
