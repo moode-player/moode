ce8845925ed3799669239f25da5b1f8a4c7443e3
Tim Curtis <tim@moodeaudio.org>
2024-11-09 17:38:37 -0500

    [sysinfo.sh] Add rpi_scntype

--- www/util/sysinfo.sh
+++ www/util/sysinfo.sh
@@ -373,6 +373,7 @@ RENDERER_SETTINGS() {
 		echo -e "\nHDMI 4K 60Hz\t\t= $hdmi_enable_4kp60\c"
 		echo -e "\nDisable GPU\t\t= $disable_gpu_chromium\c"
 		echo -e "\nChromium browser\t= $chromium_ver\c"
+		echo -e "\nScreen type\t\t= $rpi_scntype\c"
 		echo -e "\nBacklight\t\t= $rpi_backlight\c"
 		echo -e "\nBrightness\t\t= $scnbrightness\c"
 		echo -e "\nPixel aspect ratio\t= $pixel_aspect_ratio\c"
@@ -863,6 +864,7 @@ hdmi_cec=$(moodeutl -d -gv "hdmi_cec")
 hdmi_enable_4kp60=$(moodeutl -d -gv "hdmi_enable_4kp60")
 disable_gpu_chromium=$(moodeutl -d -gv "disable_gpu_chromium")
 chromium_ver=$(dpkg -l | grep -m 1 "chromium-browser" | awk '{print $3}' | cut -d":" -f 2)
+rpi_scntype=$(moodeutl -d -gv "rpi_scntype")
 rpi_backlight=$(moodeutl -d -gv "rpi_backlight")
 value=$(moodeutl -d -gv usb_volknob)
 [[ "$value" = "1" ]] && usb_volknob="On" || usb_volknob="Off"
