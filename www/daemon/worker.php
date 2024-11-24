704c6246e29f5562f4869510fea3a06bd289078b
Tim Curtis <tim@moodeaudio.org>
2024-11-24 18:12:24 -0500

    [worker..php] Fix permissions
    Moode-player package should set these but "file not found" errors occur so lets set them in worker.hp
    /etc/machine-info
    /etc/systemd/system/bluealsa.service

--- www/daemon/worker.php
+++ www/daemon/worker.php
@@ -210,6 +210,9 @@ if (!file_exists(ETC_MACHINE_INFO)) {
 	sysCmd('cp /usr/share/moode-player' . ETC_MACHINE_INFO . ' /etc/');
 	workerLog('worker: File check created default /etc/machine-info');
 }
+// Moode-player package should set these but "file not found" errors occur so lets set them here
+sysCmd('chmod 0644 ' . ETC_MACHINE_INFO);
+sysCmd('chmod 0644 ' . '/etc/systemd/system/bluealsa.service');
 workerLog('worker: File check:    complete');
 
 // Log to RAM
