8dd1e5ea61f5848c1c684197f38156299c37f26e
Tim Curtis <tim@moodeaudio.org>
2024-12-14 08:16:03 -0500

    [playerlib.js] Improve debug message

--- www/js/playerlib.js
+++ www/js/playerlib.js
@@ -1052,8 +1052,8 @@ function renderUI() {
 
         // Debug notification (appears above cover art)
         // var debugText = GLOBAL.userAgent + '<br>' + (GLOBAL.chromium ? 'chromium=true' : 'chromium=false');
-        var debugText = SESSION.json['debuglog'] == '1' ? 'Debug log on' : '';
-        debugText += SESSION.json['xss_detect'] == 'on' ? ', XSS detect on' : '';
+        var debugText = SESSION.json['debuglog'] == '1' ? 'Debug logging on' : '';
+        debugText += SESSION.json['xss_detect'] == 'on' ? ', XSS detection on' : '';
         if (debugText != '') {
             $('#debug-text').html('>> ' + debugText.replace(/^\,\ /, '') + ' <<');
         }
