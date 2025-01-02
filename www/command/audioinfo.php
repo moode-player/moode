5943b1311850ffb3931a2f085541968d772c9d18
Tim Curtis <tim@moodeaudio.org>
2025-01-02 08:24:03 -0500

    [audioinfo.php] Audio info not showing for UPnP files
    - The fix was to bump variable $numLines from 14 to 15.
    - Also refactor var $array to $tagArray

--- www/command/audioinfo.php
+++ www/command/audioinfo.php
@@ -29,24 +29,24 @@ switch ($_GET['cmd']) {
 
 // Parse station info
 function parseStationInfo($path) {
-	$array = array();
+	$tagArray = array();
 	$result = sqlQuery("SELECT * FROM cfg_radio WHERE station='" . SQLite3::escapeString($path) . "'", sqlConnect());
 
-	$array[0] = array('Logo' => LOGO_ROOT_DIR . $result[0]['name'] . '.jpg');
-	$array[1] = array('Station name' => $result[0]['name']);
-	$array[2] = array('Playable URL' => $result[0]['station']);
-	$array[3] = array('Type' => $result[0]['type'] == 'f' ? 'Favorite' : ($result[0]['type'] == 'r' ? 'Regular' : 'Hidden'));
-	$array[4] = array('Genre' => $result[0]['genre']);
-	$array[5] = array('Broadcaster' => $result[0]['broadcaster']);
-	$array[6] = array('Home page' => $result[0]['home_page']);
-	$array[7] = array('Language' => $result[0]['language']);
-	$array[8] = array('Country' => $result[0]['country']);
-	$array[9] = array('Region' => $result[0]['region']);
-	$array[10] = array('Bitrate' => $result[0]['bitrate']);
-	$array[11] = array('Audio format' => $result[0]['format']);
-	$array[12] = array('Geo fenced' => $result[0]['geo_fenced']);
+	$tagArray[0] = array('Logo' => LOGO_ROOT_DIR . $result[0]['name'] . '.jpg');
+	$tagArray[1] = array('Station name' => $result[0]['name']);
+	$tagArray[2] = array('Playable URL' => $result[0]['station']);
+	$tagArray[3] = array('Type' => $result[0]['type'] == 'f' ? 'Favorite' : ($result[0]['type'] == 'r' ? 'Regular' : 'Hidden'));
+	$tagArray[4] = array('Genre' => $result[0]['genre']);
+	$tagArray[5] = array('Broadcaster' => $result[0]['broadcaster']);
+	$tagArray[6] = array('Home page' => $result[0]['home_page']);
+	$tagArray[7] = array('Language' => $result[0]['language']);
+	$tagArray[8] = array('Country' => $result[0]['country']);
+	$tagArray[9] = array('Region' => $result[0]['region']);
+	$tagArray[10] = array('Bitrate' => $result[0]['bitrate']);
+	$tagArray[11] = array('Audio format' => $result[0]['format']);
+	$tagArray[12] = array('Geo fenced' => $result[0]['geo_fenced']);
 
-	return $array;
+	return $tagArray;
 }
 
 function parseTrackInfo($resp) {
@@ -75,12 +75,12 @@ function parseTrackInfo($resp) {
 		debugLog('parseTrackInfo(): Returned null');
 		return null;
 	} else {
-		$array = array();
+		$tagArray = array();
 		$line = strtok($resp, "\n");
-		$numLines = 14;
+		$numLines = 15;
 
 		for ($i = 0; $i < $numLines; $i++) {
-			$array[$i] = '';
+			$tagArray[$i] = '';
 		}
 
 		while ($line) {
@@ -101,35 +101,35 @@ function parseTrackInfo($resp) {
 					$file = $origValue;
 					$level = stripos(dirname($file), '.cue', -4) === false ? 1 : 2;
 					$cover_hash = md5(dirname($file, $level));
-					$array[0] = file_exists(THMCACHE_DIR . $cover_hash . '.jpg') ? array('Covers' => $cover_hash) : array('Covers' => '');
+					$tagArray[0] = file_exists(THMCACHE_DIR . $cover_hash . '.jpg') ? array('Covers' => $cover_hash) : array('Covers' => '');
 					break;
 				case 'Artist':
 				case 'Performer':
 					$artists .= $value . ', ';
 					break;
 				case 'AlbumArtist':
-					$array[3] = array('Album artist' => $value);
+					$tagArray[3] = array('Album artist' => $value);
 					break;
 				case 'Composer':
-					$array[4] = array($element => $value);
+					$tagArray[4] = array($element => $value);
 					break;
 				case 'Conductor':
-					$array[5] = array($element => $value);
+					$tagArray[5] = array($element => $value);
 					break;
 				case 'Genre':
 					$genres .= $value . ', ';
 					break;
 				case 'Album':
-					$array[7] = array($element => $value);
+					$tagArray[7] = array($element => $value);
 					break;
 				case 'Disc':
-					$array[8] = array($element => $value);
+					$tagArray[8] = array($element => $value);
 					break;
 				case 'Track':
-					$array[9] = array($element => $value);
+					$tagArray[9] = array($element => $value);
 					break;
 				case 'Title':
-					$array[10] = array($element => $value);
+					$tagArray[10] = array($element => $value);
 					break;
 				case 'Date':
 				case 'OriginalDate':
@@ -138,15 +138,15 @@ function parseTrackInfo($resp) {
 					$year = substr($value, 0, 4);
 					$month = substr($value, 4, 2);
 					$idx = $element == 'Date' ? 11 : ($element == 'OriginalDate' ? 12 : 13);
-					$array[$idx] = empty($month) ?
+					$tagArray[$idx] = empty($month) ?
 						array($element => $value) :
 						array($element => MONTH_NAME[$month] . ' ' . $year);
 					break;
 				case 'Time':
-					$array[14] = array('Duration' => formatSongTime($value));
+					$tagArray[14] = array('Duration' => formatSongTime($value));
 					break;
 				case 'Comment':
-					$array[16] = array($element => $value);
+					$tagArray[16] = array($element => $value);
 					break;
 			}
 
@@ -154,14 +154,14 @@ function parseTrackInfo($resp) {
 		}
 
 		// File path
-		$array[1] = isset($file) ? array('File path' => $file) : array('File path' => 'Not playing');
+		$tagArray[1] = isset($file) ? array('File path' => $file) : '';
 		// Artists and genres
-		$array[2] = !empty(rtrim($artists, ', ')) ? array('Artists' => rtrim($artists, ', ')) : '';
-		$array[6] = !empty(rtrim($genres, ', ')) ? array('Genres' => rtrim($genres, ', ')) : '';
+		$tagArray[2] = !empty(rtrim($artists, ', ')) ? array('Artists' => rtrim($artists, ', ')) : '';
+		$tagArray[6] = !empty(rtrim($genres, ', ')) ? array('Genres' => rtrim($genres, ', ')) : '';
 		// Audio format
 		$encodedAt = getEncodedAt(array('file' => $file, 'Format' => $format), 'verbose');
-		$array[15] = $encodedAt == 'Not playing' ? '' : array('Audio format' => $encodedAt);
+		$tagArray[15] = $encodedAt == 'Not playing' ? '' : array('Audio format' => $encodedAt);
 	}
 
-	return $array;
+	return $tagArray;
 }
