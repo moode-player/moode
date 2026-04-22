9659bdc799735b77323e41fc8a6f3e299c9e80ac
Tim Curtis <tim@moodeaudio.org>
2026-04-22 17:37:41 -0400

    [ren-status] Minor cleanup

--- www/ren-status.php
+++ www/ren-status.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * SPDX-License-Identifier: GPL-3.0-or-later
- * Copyright 2014 The moOde audio player project / Tim Curtis
+ * Copyright 2026 The moOde audio player project / Tim Curtis
 */
 
 require_once __DIR__ . '/inc/common.php';
@@ -9,7 +9,7 @@ require_once __DIR__ . '/inc/session.php';
 
 if (file_exists(PLUGIN_LOG)) {
 	$result = sysCmd('cat ' . PLUGIN_LOG);
-	$_plugin_log = 'Begin' . '<br>';
+	$_plugin_log = sysCmd('cat /var/log/moode.log | grep "Downloading"')[0] . '<br>';
 	for ($i = 0; $i < count($result); $i++) {
 		$_plugin_log .= $result[$i] . '<br>';
 	}
