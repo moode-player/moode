915b43e854cf3c37662a680575dbb6ff6158a41d
Tim Curtis <tim@moodeaudio.org>
2026-06-19 09:52:03 -0400

    [libstats.php] Remove unnecessary copyright comment

--- www/util/libstats.php
+++ www/util/libstats.php
@@ -3,7 +3,6 @@
 /*
  * SPDX-License-Identifier: GPL-3.0-or-later
  * Copyright 2014 The moOde audio player project / Tim Curtis
- * Copyright 2015 Cover art extraction routines / Andreas Goetz (cpuidle@gmx.de)
 */
 
 require_once __DIR__ . '/../inc/common.php';
