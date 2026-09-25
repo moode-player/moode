626186db784ca14fc8311a9e51018cfbfd4f5112
Tim Curtis <tim@moodeaudio.org>
2026-09-25 17:24:27 -0400

    [moode-sqlite3.db] Bump to v5-moode-meters

--- var/local/www/db/moode-sqlite3.db.sql
+++ var/local/www/db/moode-sqlite3.db.sql
@@ -1,5 +1,5 @@
 --
--- File generated with SQLiteStudio v3.4.4 on Sun Sep 20 08:32:05 2026
+-- File generated with SQLiteStudio v3.4.4 on Fri Sep 25 10:22:49 2026
 --
 -- Text encoding used: UTF-8
 --
@@ -238,7 +238,7 @@ CREATE TABLE cfg_outputdev (id INTEGER PRIMARY KEY, device_name CHAR (32), mpd_v
 -- Table: cfg_plugin
 CREATE TABLE cfg_plugin (id INTEGER PRIMARY KEY, component CHAR (32), type CHAR (32), plugin CHAR (32), version CHAR (32));
 INSERT INTO cfg_plugin (id, component, type, plugin, version) VALUES (1, 'camilladsp', 'sample-configs', 'v4-sample-configs', '4.0.0');
-INSERT INTO cfg_plugin (id, component, type, plugin, version) VALUES (2, 'peppydisplay', 'moode-meters', 'v4-moode-meters', '4.0.0');
+INSERT INTO cfg_plugin (id, component, type, plugin, version) VALUES (2, 'peppydisplay', 'moode-meters', 'v5-moode-meters', '5.0.0');
 INSERT INTO cfg_plugin (id, component, type, plugin, version) VALUES (3, 'renderer', 'airplay', 'v5-shairport-sync', '5.5.1-1moode1');
 INSERT INTO cfg_plugin (id, component, type, plugin, version) VALUES (4, 'renderer', 'spotify-connect', 'v8-librespot', '0.8.0-1moode1');
 INSERT INTO cfg_plugin (id, component, type, plugin, version) VALUES (5, 'renderer', 'qobuz-connect', 'v2-pibuz', '2.4.1-1moode1');
